0e6a9ca05422927f87f2e25005556b5644e4b917
Pala <Pala@Palade-MacBook-Pro.local>
2016-05-09 23:40:24 +0800

    Google maps api

--- resources/js/script.js
+++ resources/js/script.js
@@ -74,4 +74,22 @@ $(document).ready(function() {
             icon.removeClass('ion-close-round');
         }
     });        
+
+    // Maps
+    var map = new GMaps({
+      div: '.map',
+      lat: 38.7436057,
+      lng: -9.1,
+      zoom: 12
+    });
+
+    map.addMarker({
+      lat: 38.7436057,
+      lng: -9.2302439,
+      title: 'Lisbon',
+        infoWindow: {
+          content: '<p>Our Lisbon HQ</p>'
+        }
+    });
+
 });
